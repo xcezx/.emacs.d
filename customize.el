0363a4eefde58eaf4b2e156f12f5bad61065e684
xcezx <main.xcezx@gmail.com>
2014-10-11 00:47:42 +0900

    init

---
+++ customize.el
@@ -0,0 +1,57 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-delay 0.3)
+ '(after-save-hook (quote (executable-make-buffer-file-executable-if-script-p)))
+ '(column-number-mode t)
+ '(custom-safe-themes (quote ("a507b9ca4a605d5256716da70961741b9ef9ec3246041a4eb776102e8df18418" default)))
+ '(delete-selection-mode t)
+ '(delete-trailing-lines t)
+ '(dired-dwim-target t)
+ '(dired-isearch-filenames t)
+ '(dired-recursive-copies (quote always))
+ '(dired-use-ls-dired nil)
+ '(electric-pair-mode t)
+ '(electric-pair-pairs (quote ((34 . 34))))
+ '(emacs-lisp-mode-hook (quote (hs-minor-mode turn-on-eldoc-mode)))
+ '(global-auto-revert-mode t)
+ '(helm-buffer-max-length 64)
+ '(history-delete-duplicates t)
+ '(hs-minor-mode-hook (quote (xcezx/hs-config)))
+ '(html-mode-hook (quote (emmet-mode)))
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice t)
+ '(js-indent-level 2)
+ '(kill-do-not-save-duplicates t)
+ '(kill-whole-line t)
+ '(make-backup-files nil)
+ '(menu-bar-mode nil)
+ '(multi-web-global-mode t nil (multi-web-mode))
+ '(mweb-default-major-mode (quote html-mode))
+ '(mweb-filename-extensions (quote ("html" "twig" "tpl")))
+ '(mweb-submode-indent-offset 4)
+ '(mweb-tags (quote ((php-mode "<\\?php\\|<\\?=" "\\?>") (js-mode "<script[^>]*>" "</script>") (css-mode "<style[^>]*>" "</style>"))))
+ '(php-mode-coding-style (quote psr2))
+ '(php-mode-hook (quote (php-eldoc-enable helm-gtags-mode hs-minor-mode)))
+ '(php-mode-warn-if-mumamo-off nil)
+ '(read-buffer-completion-ignore-case t)
+ '(recentf-mode t)
+ '(safe-local-variable-values (quote ((py-indent-offset . 4))))
+ '(scroll-bar-mode nil)
+ '(sgml-basic-offset 4)
+ '(sgml-mode-hook (quote (emmet-mode)))
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(split-width-threshold nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bm-face ((t (:background "green" :foreground "black"))) t)
+ '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-error :background "red" :foreground "yellow" :weight normal))) t)
+ '(helm-selection ((t (:background "brightred" :underline t))))
+ '(mode-line ((t (:background "#212931" :foreground "black" :box (:line-width -1 :style released-button))))))
---
+++ init.el
@@ -0,0 +1,150 @@
+;; Package
+(setq package-user-dir (locate-user-emacs-file "vendor")
+      package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+(unless (package-installed-p 'package+)
+  (package-refresh-contents)
+  (package-install 'package+))
+(package-manifest
+ 'auto-complete
+ 'auto-save-buffers-enhanced
+ 'bm
+ 'direx
+ 'e2wm
+ 'emmet-mode
+ 'flycheck
+ 'flycheck-color-mode-line
+ 'helm
+ 'helm-gtags
+ 'magit
+ 'markdown-mode
+ 'multi-web-mode
+ 'org
+ 'package+
+ 'php-eldoc
+ 'php-mode
+ 'popwin
+ 'yaml-mode
+ )
+
+
+;; Server
+(when (require 'server nil t)
+  (unless (server-running-p) (server-start)))
+
+;; Customize
+(setq custom-file (locate-user-emacs-file "customize.el"))
+(load custom-file t)
+
+;; Theme
+(load-theme 'misterioso t)
+
+
+(setq system-time-locale "C")
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq-default indent-tabs-mode nil)
+
+(defun xcezx/make-scratch-buffer (&optional arg)
+  (set-buffer (get-buffer-create "*scratch*"))
+  (funcall initial-major-mode)
+  (erase-buffer)
+  (when initial-scratch-message (insert initial-scratch-message))
+  (when arg (switch-to-buffer "*scratch*"))
+  (message "*scratch* is created.")
+  nil)
+
+;;
+(defun xcezx/make-scratch-buffer-on-kill-buffer-query-functions ()
+  (if (string= "*scratch*" (buffer-name))
+      (xcezx/make-scratch-buffer t)
+    t))
+(add-hook 'kill-buffer-query-functions 'xcezx/make-scratch-buffer-on-kill-buffer-query-functions)
+
+(defun xcezx/make-scratch-buffer-on-after-save-hook ()
+  (unless (member "*scratch*" (mapcar 'buffer-name (buffer-list)))
+    (xcezx/make-scratch-buffer)))
+(add-hook 'after-save-hook 'xcezx/make-scratch-buffer-on-after-save-hook)
+
+;;
+(defun xcezx/make-messages-buffer ()
+  (set-buffer (get-buffer-create "*Messages*"))
+  (fundamental-mode)
+  (erase-buffer)
+  (message "*Messages* is created.")
+  nil)
+(defun xcezx/make-messages-buffer-on-kill-buffer-query-functions ()
+  (if (string= "*Messages*" (buffer-name))
+      (xcezx/make-messages-buffer)
+    t))
+(add-hook 'kill-buffer-query-functions 'xcezx/make-messages-buffer-on-kill-buffer-query-functions)
+
+;;
+(defun kill-region-or-backward-kill-word (arg)
+  (interactive "p")
+  (if mark-active
+      (call-interactively 'kill-region)
+    (backward-kill-word arg)))
+(define-key global-map (kbd "C-w") 'kill-region-or-backward-kill-word)
+
+(define-key global-map (kbd "C-x ,") (lambda () (interactive) (find-file user-init-file)))
+(define-key global-map (kbd "C-h") 'delete-backward-char)
+(define-key global-map (kbd "C-o") 'dabbrev-expand)
+(define-key global-map (kbd "C-x .") (lambda () (interactive) (eval-current-buffer) (message "Evaluated current buffer")))
+(define-key global-map (kbd "C-c C-a") 'align-regexp)
+(define-key global-map (kbd "C-x C-b") 'electric-buffer-list)
+(define-key global-map (kbd "C-x C-d") 'dired-jump-other-window)
+(define-key global-map (kbd "C-x p") (lambda () (interactive) (other-window -1)))
+(define-key minibuffer-local-completion-map (kbd "C-w") 'backward-kill-word)
+
+(defadvice move-beginning-of-line (around adviced-beginning-of-line (arg) activate)
+  (if (bolp)
+      (back-to-indentation)
+    ad-do-it))
+
+(defadvice beginning-of-visual-line (around adviced-beginning-of-visual-line (&optional N) activate)
+  (if (bolp)
+      (back-to-indentation)
+    ad-do-it))
+
+(defadvice after-find-file (before adviced-after-find-file activate)
+  "Make the directory containing the visited file."
+  (make-directory (file-name-directory (buffer-file-name)) t))
+
+(defun xcezx/hs-config ()
+  (define-key hs-minor-mode-map (kbd "C-c h c") 'hs-toggle-hiding)
+  (define-key hs-minor-mode-map (kbd "C-c h h") 'hs-hide-block)
+  (define-key hs-minor-mode-map (kbd "C-c h s") 'hs-show-block)
+  (define-key hs-minor-mode-map (kbd "C-c h H") 'hs-hide-all)
+  (define-key hs-minor-mode-map (kbd "C-c h S") 'hs-show-all)
+  (define-key hs-minor-mode-map (kbd "C-c h l") 'hs-hide-level))
+
+(when (require 'wdired nil t)
+  (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode))
+
+
+(ac-config-default)
+
+(auto-save-buffers-enhanced t)
+
+(helm-mode 1)
+(define-key global-map (kbd "C-x ;") 'helm-mini)
+(define-key global-map (kbd "C-x b") 'helm-buffers-list)
+
+(defun xcezx/helm-gtags-mode-hook ()
+  (define-key helm-gtags-mode-map (kbd "C-x g") 'helm-gtags-select))
+(add-hook 'helm-gtags-mode-hook 'xcezx/helm-gtags-mode-hook)
+
+(define-key global-map (kbd "M-1") 'bm-toggle)
+(define-key global-map (kbd "M-2") 'bm-previous)
+(define-key global-map (kbd "M-3") 'bm-next)
+
+(when (require 'popwin nil t)
+  (popwin-mode 1)
+  (push '(direx:direx-mode :position left :width 50 :dedicated t)
+        popwin:special-display-config))
+(define-key global-map (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
+
+
+;;; init.el ends here
