b814a05025d7fe1ec12cd770575a4870328cc91c
xcezx <main.xcezx@gmail.com>
2015-12-08 23:01:05 +0900

    customize mode-line face

--- customize.el
+++ customize.el
@@ -82,4 +82,4 @@
  '(helm-selection ((t (:background "brightred" :underline t))))
  '(magit-diff-added-highlight ((t (:inherit diff-added))))
  '(magit-diff-removed-highlight ((t (:inherit diff-removed))))
- '(mode-line ((t (:background "#212931" :foreground "black" :box (:line-width -1 :style released-button))))))
+ '(mode-line ((t (:background "cyan" :foreground "green")))))
