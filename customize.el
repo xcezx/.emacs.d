89c34e55c47c89288ce0b41f8677c60bbaf84a7f
xcezx <main.xcezx@gmail.com>
2016-11-01 12:53:42 +0900

    add origami-mode on yaml-mode-hook

--- customize.el
+++ customize.el
@@ -76,7 +76,7 @@
  '(tumblesocks-post-default-state "ask")
  '(tumblesocks-token-file "~/Dropbox/Private/tumblr-oauth-token")
  '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify))
- '(yaml-mode-hook (quote (yaml-set-imenu-generic-expression))))
+ '(yaml-mode-hook (quote (yaml-set-imenu-generic-expression origami-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
