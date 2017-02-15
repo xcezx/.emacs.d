de7ecc6c0488a686667b92d61a80e1902868f45a
xcezx <main.xcezx@gmail.com>
2017-02-15 14:11:52 +0900

    --

--- customize.el
+++ customize.el
@@ -38,7 +38,7 @@
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(initial-buffer-choice t)
- '(js-indent-level 4)
+ '(js-indent-level 2)
  '(js2-init-hook (quote (hs-minor-mode)))
  '(kill-do-not-save-duplicates t)
  '(kill-whole-line t)
